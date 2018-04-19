cfa01820ad9f88a61e15a3439be594e9d3f6b1e6
Andrii Hrynenko <pathliving@gmail.com>
2018-04-19 22:24:07 +0300

    Adaptive design, cross-browser testing, code refactoring and other
    1. Code refactoring  (HTML, CSS, JS)
    2. Add adaptive design with @media queries
    3. Cross-browser testing (Chrome, Firefox, Opera, Edge, IE)
    4. Hide content from IE and add a recommendation to download another browser (<noscript></noscript>)
    5. Hide save button on Firefox, IE and Edge browsers because don't work
    6. Add comments to HTML, CSS, JS

--- 8-bit/main.js
+++ 8-bit/main.js
@@ -1,46 +1,48 @@
-/* toggle to visible or hidden board with settings */
+/* Toggle to visible or hidden board with settings */
 $(".settings").on("click", function () {
-  $(".board").toggle(".visible-brd")
+  $(".board").toggle(".visible-brd");
 });
 
 $(".close").on("click", function() {
-  $(".board").toggle(".visible-brd")
+  $(".board").toggle(".visible-brd");
 });
 
-/* toggle to visible or hidden custom grid size */
+/* Toggle to visible or hidden custom grid size */
 $(".custom").on("click", function() {
-  $("form").toggle()
+  $("form").toggle();
 });
 
-/* generation pixel canvas */
-function makeGrid (height, width) {
-    
+/* Generation pixel canvas */
+function makeGrid (height, width) {    
   $("#pixelCanvas").html("");
   
   for (var h = 0; h < height; h++) {  
-  $("#pixelCanvas").append("<tr></tr>");
+    $("#pixelCanvas").append("<tr></tr>");
   }
 
   $("tr").each(function() {
     for(var w = 0; w < width; w++) {
       $(this).append("<td></td>");
-      }
-    });
+    }
+  });
 }
 
 $(document).ready(function() {
   $(".small").on("click", function(e) {
-  e.preventDefault();
-  makeGrid(35,35);
-});
+    e.preventDefault();
+    makeGrid(35,35);
+  });
+
   $(".medium").on("click", function(e) {
     e.preventDefault();
     makeGrid(45,55);
   });
+
   $(".large").on("click", function(e) {
     e.preventDefault();
     makeGrid(50,75);
   });
+
   $("#sizePicker").on("submit", function(e) {
     e.preventDefault();
     var height = $("#inputHeight").val();
@@ -49,40 +51,41 @@ $(document).ready(function() {
   });
 });
 
-/* click and move to add color to multiple cells */
+/* Click and move to add color to multiple cells */
 $("#pixelCanvas").on("mousedown", "td", function(event) {
    event.preventDefault();   
   
-  if (event.buttons === 1) {     
-    
+  if (event.buttons === 1) {    
     var colorCell = $("#colorPicker").val();
     $(this).attr("bgcolor", colorCell);
     
-     $(event.target).css('background-color', colorCell);       
-     $("#pixelCanvas").on('mouseenter', 'td', function(evt){ 
-        $(evt.target).css('background-color', colorCell); 
-     });  
-     $("#pixelCanvas").on('mouseup mouseleave', function(evt) {
-        evt.preventDefault(); 
-        $("#pixelCanvas").off('mouseenter');
-        return false;
-     });
-}
+    $(event.target).css('background-color', colorCell); 
+
+    $("#pixelCanvas").on('mouseenter', 'td', function(e){ 
+      $(e.target).css('background-color', colorCell); 
+    });  
+
+    $("#pixelCanvas").on('mouseup mouseleave', function(e) {
+      e.preventDefault(); 
+      $("#pixelCanvas").off('mouseenter');
+      return false;
+    });
+  }
 });
 
-/* reset button */
+/* Reset button */
 $(".reset").on("click", function (e){
   e.preventDefault();
   $("td").css("background-color", "#fff");
 });
 
-/* toggle grid button */
+/* Toggle grid button */
 $(".toggle-grid").on("click", function (e){
   e.preventDefault();
   $("tr, td").toggleClass("toggle-border");
 });
 
-/* function for converting rgb() to hex color */
+/* Function for converting rgb() to hex color */
 function searchRGB() {
   let i = colorPicked;
   let rgbValues = i.split("(")[1].split(")")[0].split(", ");
@@ -98,19 +101,19 @@ function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-$('.pickColor').on('click', function(evt) {
+$('#colors').on('click', function(evt) {
   colorPicked = $(evt.target).css("background-color");
   $('#colorPicker').val(rgbToHex(searchRGB()[0], searchRGB()[1], searchRGB()[2]));
 });
 
-/* function to save table canvas as .PNG file */
+/* Function to save table canvas as .PNG file */
 $('.save').click(function(){
-    html2canvas($("#pixelCanvas").get(0), {
-      onrendered: function (canvas) {
-        var a = document.createElement('a');
-        a.href = canvas.toDataURL("image/png");
-        a.download = 'MyPixelArt.png';
-        a.click();
+  html2canvas($("#pixelCanvas").get(0), {
+    onrendered: function (canvas) {
+      var a = document.createElement('a');
+      a.href = canvas.toDataURL("image/png");
+      a.download = 'MyPixelArt.png';
+      a.click();
     }
   });
 });
