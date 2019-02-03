6c9cd70e2e2fe99726b25970a0dcde0d46a88f9d
Andrii Hrynenko <pathliving@gmail.com>
2019-02-04 00:21:57 +0200

    Fix markup

--- main.js
+++ main.js
@@ -1,98 +1,98 @@
 /* Toggle to visible or hidden board with settings */
 $(".settings").on("click", function () {
-  $(".board").toggle(".visible-brd");
+	$(".board").toggle(".visible-brd");
 });
 
 $(".close").on("click", function() {
-  $(".board").toggle(".visible-brd");
+	$(".board").toggle(".visible-brd");
 });
 
 $(document).ready(function() {
-  $(".small").on("click", function(e) {
-    e.preventDefault();
-    makeGrid(35,35);
-  });
+	$(".small").on("click", function(e) {
+		e.preventDefault();
+		makeGrid(35,35);
+	});
 
-  $(".medium").on("click", function(e) {
-    e.preventDefault();
-    makeGrid(45,55);
-  });
+	$(".medium").on("click", function(e) {
+		e.preventDefault();
+		makeGrid(45,55);
+	});
 
-  $(".large").on("click", function(e) {
-    e.preventDefault();
-    makeGrid(50,75);
-  });
+	$(".large").on("click", function(e) {
+		e.preventDefault();
+		makeGrid(50,75);
+	});
 
-  $("#sizePicker").on("submit", function(e) {
-    e.preventDefault();
-    var height = $("#inputHeight").val();
-    var width = $("#inputWidth").val();
-    makeGrid(height, width);
-  });
+	$("#sizePicker").on("submit", function(e) {
+		e.preventDefault();
+		var height = $("#inputHeight").val();
+		var width = $("#inputWidth").val();
+		makeGrid(height, width);
+	});
 });
 
 /* Toggle to visible or hidden custom grid size */
 $(".custom").on("click", function() {
-  $("form").toggle();
+	$("form").toggle();
 });
 
 /* Generation pixel canvas */
 function makeGrid (height, width) {    
-  $("#pixelCanvas").html("");
-  
-  for (var h = 0; h < height; h++) {  
-    $("#pixelCanvas").append("<tr></tr>");
-  }
+	$("#pixelCanvas").html("");
+	
+	for (var h = 0; h < height; h++) {  
+		$("#pixelCanvas").append("<tr></tr>");
+	}
 
-  $("tr").each(function() {
-    for(var w = 0; w < width; w++) {
-      $(this).append("<td></td>");
-    }
-  });
+	$("tr").each(function() {
+		for(var w = 0; w < width; w++) {
+		$(this).append("<td></td>");
+		}
+	});
 }
 
 /* Click and move to add color to multiple cells */
 $("#pixelCanvas").on("mousedown", "td", function(event) {
-   event.preventDefault();   
-  
-  if (event.buttons === 1) {    
-    var colorCell = $("#colorPicker").val();
-    $(this).attr("bgcolor", colorCell);
-    
-    $(event.target).css("background-color", colorCell); 
+	event.preventDefault();   
+	
+	if (event.buttons === 1) {    
+		var colorCell = $("#colorPicker").val();
+		$(this).attr("bgcolor", colorCell);
+		
+		$(event.target).css("background-color", colorCell); 
 
-    $("#pixelCanvas").on("mouseenter", "td", function(e){ 
-      $(e.target).css("background-color", colorCell); 
-    });  
+		$("#pixelCanvas").on("mouseenter", "td", function(e) { 
+			$(e.target).css("background-color", colorCell); 
+		});  
 
-    $("#pixelCanvas").on("mouseup mouseleave", function(e) {
-      e.preventDefault(); 
-      $("#pixelCanvas").off("mouseenter");
-      return false;
-    });
-  }
+		$("#pixelCanvas").on("mouseup mouseleave", function(e) {
+			e.preventDefault(); 
+			$("#pixelCanvas").off("mouseenter");
+			return false;
+		});
+	}
 });
 
 /* Reset button */
-$(".reset").on("click", function (e){
-  e.preventDefault();
-  $("td").css("background-color", "#fff");
+$(".reset").on("click", function (e) {
+	e.preventDefault();
+	$("td").css("background-color", "#fff");
 });
 
 /* Toggle grid button */
-$(".toggle-grid").on("click", function (e){
-  e.preventDefault();
-  $("tr, td").toggleClass("toggle-border");
+$(".toggle-grid").on("click", function (e) {
+	e.preventDefault();
+	$("tr, td").toggleClass("toggle-border");
 });
 
 /* Function to save table canvas as .PNG file */
-$(".save").click(function(){
-  html2canvas($("#pixelCanvas").get(0), {
-    onrendered: function (canvas) {
-      var a = document.createElement("a");
-      a.href = canvas.toDataURL("image/png");
-      a.download = "MyPixelArt.png";
-      a.click();
-    }
-  });
+$(".save").click(function() {
+	html2canvas($("#pixelCanvas").get(0), {
+		onrendered: function (canvas) {
+			var a = document.createElement("a");
+			a.href = canvas.toDataURL("image/png");
+			a.download = "MyPixelArt.png";
+			a.click();
+		}
+	});
 });
